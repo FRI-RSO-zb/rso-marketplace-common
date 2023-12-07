3ed67fa0485acb68683c7e472c36c9cb5d14dd69
Ziga Bobnar <ziga@bobnar.net>
2023-12-07 15:30:47 +0100

    Add dtos

---
+++ src/main/java/dtos/loaderAgent/v1/processors/ProcessingResult.java
@@ -0,0 +1,10 @@
+package dtos.loaderAgent.v1.processors;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(description = "Result of processing operation.")
+public record ProcessingResult(
+        Boolean isSuccessful,
+        String errors,
+        Object result
+) {}
